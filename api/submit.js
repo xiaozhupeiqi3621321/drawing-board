09770d5df9b7d2d7298cd6ab82a671f6a9ca6f26
xiaozhupeiqi3621321 <1783253847@qq.com>
2026-06-09 19:16:47 +0800

    fix: add allowOverwrite: true to put()

--- api/submit.js
+++ api/submit.js
@@ -29,6 +29,7 @@ export default async function handler(req, res) {
       contentType: 'application/json',
       access: 'private',
       addRandomSuffix: false,
+      allowOverwrite: true,
     });
 
     return res.json({ ok: true, id });
